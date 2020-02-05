20447a9453b58a9cc3b4f1c3b0627355cbcb7aed
dyjin <dyjin1217@naver.com>
2020-02-05 22:25:26 +0900

    project init

---
+++ todolist/todolist_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+
+# Create your views here.
+
+def index(request):
+    todos = Todo.objects.all()
+    content = {'todos' : todos}
+    return render(request, 'todolist_app/index.html',content)
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def deleteTodo(request):
+    delete_todo_id =  request.GET['todoNum']
+    print("완료한 todo의 id", delete_todo_id)
+    todo = Todo.objects.get(id= delete_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def doneTodo(request):
+    done_todo_id =  request.GET['todoNum']
+    print("완료한 todo의 id", done_todo_id)
+    todo = Todo.objects.get(id= done_todo_id)
+    todo.isDone=True
+    todo.save()
+    return HttpResponseRedirect(reverse('index'))
